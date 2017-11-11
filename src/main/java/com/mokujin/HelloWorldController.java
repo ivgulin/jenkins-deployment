92ac34c6778aee42f71d8146dc4ce8d2e1ce7aee
Vania <boringnameishere@gmail.com>
2017-11-11 20:40:53 +0200

    initial commit

---
+++ src/main/java/com/mokujin/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.mokujin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String sayHello(){
+        return "Hello World1";
+    }
+}
---
+++ src/main/java/com/mokujin/JenkinsDeploymentApplication.java
@@ -0,0 +1,12 @@
+package com.mokujin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDeploymentApplication.class, args);
+	}
+}
