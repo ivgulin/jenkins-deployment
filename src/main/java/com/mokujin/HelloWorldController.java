e8f9abefec0c15a234cf515274c79e278ce7213b
Vania <boringnameishere@gmail.com>
2017-11-12 13:57:19 +0200

    changes

--- src/main/java/com/mokujin/HelloWorldController.java
+++ src/main/java/com/mokujin/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
 
     @RequestMapping("/")
     public String sayHello(){
-        System.out.println("Hello world!");
+        System.out.println("Hello world!!");
         return "Hello World1!!";
     }
 }
